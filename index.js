f82f02e216a5af39e13374316bb89106974e4adb
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-13 17:47:21 -0300

    Including index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'azk-core': require('./lib/src/azk-core'),
+	'config'  : require('./lib/src/config'),
+	'i18n'    : require('./lib/src/i18n'),
+	'utils'   : require('./lib/src/utils')
+};
