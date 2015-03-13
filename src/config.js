88fb79307fb265aa4c0a5aa0b3ffc12fc2bc49db
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-13 17:37:43 -0300

    Updating config

--- src/config.js
+++ src/config.js
@@ -1,153 +1,16 @@
 import { _, envs, mergeConfig } from './utils';
-var path = require('path');
-var os   = require('os');
-
-// Configs paths
-var azk_root  = envs('AZK_ROOT_PATH', '');
-var data_path = envs('AZK_DATA_PATH', '$HOME/.azk/data');
-
-// Namespace to support multiple instances of the agent
-var namespace = envs('AZK_NAMESPACE');
-
-// Use virtual machine or not?
-var default_vm  = os.platform() == "linux" ? "false" : "true";
-var requires_vm = (envs('AZK_USE_VM', default_vm) == "true");
-
-// Persistent folder path
-var persistent_folders = requires_vm ?
-  '/mnt/sda1/azk/persistent_folders' :
-  path.join(data_path, 'persistent_folders');
-
-// Paths to use in config
-var paths = {
-  logs: path.join(data_path, 'logs'),
-  run : path.join(data_path, 'run'),
-  vm  : path.join(data_path, 'vm'),
-};
-
-var dns_nameservers = function(key, defaultValue) {
-  var value = envs(key);
-  return _.isEmpty(value) ? defaultValue : _.invoke(value.split(','), 'trim');
-};
 
 class Dynamic {
   constructor(key) { this.key = key; }
 }
 
-// Dir name used by manifest meta
-var azk_dir = '.azk';
-
-var options = mergeConfig({
-  '*': {
-    namespace: namespace,
-    manifest : "Azkfile.js",
-    locale   : 'en-US',
-    azk_dir  : azk_dir,
-    flags    : {
-      show_deprecate: (envs('AZK_HIDE_DEPRECATE') != 'true'),
-    },
-    paths    : {
-      azk_root,
-      data  : data_path,
-      logs  : paths.logs,
-      log   : path.join(paths.logs, 'azk.log'),
-      shared: path.join(azk_root, "shared"),
-
-      azk_meta          : path.join(data_path, azk_dir, "shared", "Azkfile.js"),
-      pems              : path.join(paths.vm , '.docker'),
-      agent_pid         : path.join(paths.run, 'agent.pid'),
-      memcached_pid     : path.join(paths.run, 'memcachedjs.pid'),
-      hipache_pid       : path.join(paths.run, 'hipache.pid'),
-      agent_socket      : path.join(paths.run, 'agent.socket'),
-      balancer_file     : path.join(paths.run, 'hipache.json'),
-      memcached_socket  : path.join(paths.run, 'memcachedjs.socket'),
-      api_socket        : path.join(paths.run, 'api.socket'),
-      persistent_folders: persistent_folders,
-    },
-    logs_level: {
-      console: (envs('AZK_DEBUG') ? 'debug' : 'warn'),
-      file: envs('AZK_LOG_LEVEL', 'info'),
-    },
-    docker: {
-      socket        : envs('AZK_DOCKER_SOCKER', "/var/run/docker.sock"),
-      host          : new Dynamic("docker:host"),
-      namespace     : envs('AZK_NAMESPACE'),
-      repository    : 'azk',
-      default_domain: 'azk',
-      build_name    : 'azkbuild',
-      image_default : 'azukiapp/azktcl:0.0.2',
-      run: {
-        timeout: 1000,
-        retry: 10,
-      }
-    },
-    agent: {
-      requires_vm: requires_vm,
-      portrange_start: 11000,
-      balancer: {
-        ip  : new Dynamic("agent:balancer:ip"),
-        host: envs('AZK_BALANCER_HOST'),
-        port: envs('AZK_BALANCER_PORT', 80),
-        file_dns: "/etc/resolver/" + envs('AZK_BALANCER_HOST'),
-      },
-      dns: {
-        ip  : new Dynamic("agent:dns:ip"),
-        port: envs('AZK_DNS_PORT', '53'),
-        nameservers  : dns_nameservers('AZK_DNS_SERVERS', []),
-        defaultserver: dns_nameservers('AZK_DNS_SERVERS_DEFAULTS', ['8.8.8.8', '8.8.4.4']),
-      },
-      vm: {
-        wait_ready : 180000,
-        ip         : envs('AZK_AGENT_VM_IP'  , '192.168.50.4'),
-        name       : envs('AZK_AGENT_VM_NAME', "azk-vm-" + namespace),
-        user       : "docker",
-        password   : "live",
-        cpus       : envs('AZK_VM_CPUS', os.cpus().length),
-        memory     : envs('AZK_VM_MEMORY', Math.floor(os.totalmem() / 1024 / 1024 / 4)),
-        ssh_key    : envs('AZK_AGENT_VM_KEY', path.join(paths.vm, "azkvm_rsa")),
-        screen_path: path.join(paths.vm, "screens"),
-        data_disk  : path.join(paths.vm, "azk-agent.vmdk"),
-        boot_disk  : path.join(envs('AZK_LIB_PATH', ''), "vm", "azk.iso"),
-        blank_disk : path.join(envs('AZK_LIB_PATH', ''), "vm", "azk-agent.vmdk.gz"),
-        mount_point: '/media/sf_Root',
-        authorized_key: '/home/docker/.ssh/authorized_keys',
-      },
-
-      // Used to carry global configuration switches the agent
-      config_keys: [],
-    },
-
-    urls: {
-      github: {
-        api:{
-          url: "https://api.github.com/repos/azukiapp/azk",
-          tags_url: "https://api.github.com/repos/azukiapp/azk/tags",
-        }
-      }
-    },
-  },
-  test: {
-    paths: {
-      log : path.join(paths.logs, 'azk_test.log'),
-    },
-    docker: {
-      namespace   : 'azk.test',
-      repository  : 'azk_test',
-      build_name  : 'azkbuildtest',
-      image_empty : 'cevich/empty_base_image',
-    },
-    agent: {
-      portrange_start: 12000,
-      vm: {
-        data_disk : path.join(paths.vm, "azk-agent-spec.vmdk"),
-      }
-    }
-  },
-});
-
 function env() {
   return envs('NODE_ENV', 'production');
 }
+
+var options = mergeConfig({
+  '*': {},
+});
 
 export function get(key) {
   if (key == "env") {
